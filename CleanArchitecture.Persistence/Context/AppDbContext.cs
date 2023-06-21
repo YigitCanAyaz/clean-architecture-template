103f993470071fb6b8185eb260c4153a247de891
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-06-21 13:41:47 +0300

    Add project files.

---
+++ CleanArchitecture.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Context;
+
+public sealed class AppDbContext : DbContext
+{
+    //AppDbContext context = new();
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer("");
+    //}
+
+    // best practice budur
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // çağırmak istersek => private readonly AppDbContext _context, sonra constructor'dan eşleştir.
+}
