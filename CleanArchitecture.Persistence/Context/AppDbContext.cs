6be5e03bae46c469b93a4b84e773a9b3f74c7d35
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-06-21 16:40:54 +0300

    CQRS (MediatR) + AutoMapper + FluentValidation integration

--- CleanArchitecture.Persistence/Context/AppDbContext.cs
+++ CleanArchitecture.Persistence/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchitecture.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Persistence.Context;
 
@@ -13,7 +14,27 @@ public sealed class AppDbContext : DbContext
     // best practice budur
     public AppDbContext(DbContextOptions options) : base(options)
     {
+        // çağırmak istersek => private readonly AppDbContext _context, sonra constructor'dan eşleştir.
     }
 
-    // çağırmak istersek => private readonly AppDbContext _context, sonra constructor'dan eşleştir.
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<Entity>();
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(p => p.CreatedDate).CurrentValue = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(p => p.UpdatedDate).CurrentValue = DateTime.Now;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,15 +1,29 @@
+﻿using CleanArchitecture.Application.Behaviors;
+using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Services;
+using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 string connectionString = builder.Configuration.GetConnectionString("SqlServer");
 
+
+builder.Services.AddScoped<ICarService, CarService>();
+
+builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistence.AssemblyReference).Assembly);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers()
-    .AddApplicationPart(typeof(
-    CleanArchitecture.Presentation.AssemblyReference).Assembly);
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
+builder.Services.AddMediatR(cfr => cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly));
+
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+// hangi katmanın assemblyisini kullanacağımızı (kurduğumuzu yazıyoruz)
+builder.Services.AddValidatorsFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
